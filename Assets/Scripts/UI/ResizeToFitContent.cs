b13bb77868945713500b834151d0c92a1e46d4c1
James Wright <jow5@case.edu>
2015-04-03 01:13:02 -0400

    Update to get the code line content panel to resize correctly to it's children. Scrollview now works correctly.
    Signed-off-by: James Wright <jow5@case.edu>

---
+++ Assets/Scripts/UI/ResizeToFitContent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeToFitContent : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        float totalHeight = 0f;
+        foreach (Transform t in GetComponentInChildren<Transform>())
+        {
+            RectTransform r = (RectTransform)t;
+            totalHeight += r.rect.height;
+        }
+        float width = ((RectTransform)transform).rect.width;
+        
+	}
+}
