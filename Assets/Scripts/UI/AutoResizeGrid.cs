c3546bc463dbff42703c9af196e8369a706c9259
James Wright <jow5@case.edu>
2015-04-03 00:58:46 -0400

    Update to setup a scrollable grid view for the components. Moved scripts into their correct folders.
    Signed-off-by: James Wright <jow5@case.edu>

---
+++ Assets/Scripts/UI/AutoResizeGrid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class AutoResizeGrid : MonoBehaviour {
+
+    public GridLayoutGroup grid;
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(((RectTransform)transform).rect);
+        grid.cellSize = new Vector2(((RectTransform)transform).rect.width, grid.cellSize.y);
+	}
+}
