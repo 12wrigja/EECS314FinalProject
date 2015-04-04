9e7b8e5a5fd8e652efb7e23eac6d121cc5a2f39e
Haley <hne3@case.edu>
2015-04-04 18:42:08 -0400

    Updated tutorial and welcome screen
    Tutorial now reads from a text file and plays sound on text proceed.
    Also has background music.
    Welcome screen now loads tutorial upon space bar press and plays
    affirmative noise.

---
+++ Assets/Scripts/LoadScreenScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadScreenScript : MonoBehaviour {
+
+	public AudioClip proceed;
+	public Text clickText;
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			clickText.gameObject.SetActive(false); // Gets rid of "tap to start" text.
+			GetComponent<AudioSource>().PlayOneShot(proceed, 20f); // Plays proceed sound.
+			for(int i = 0; i < 500000; i++){ // Stall loop to give noise time to play.
+				for(int j = 0; j < 120; j++){
+				}
+			}
+			Application.LoadLevel(1); // Loads tutorial. EDIT: Will eventually load selection screen.
+		}
+	}
+}
---
+++ Assets/Scripts/TextReader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class TextReader : MonoBehaviour {
+
+	public string scriptAddress;
+	public Text tutorialText;
+	public AudioClip buttonClip;
+	private string nextLine;
+	private StreamReader reader;
+	private bool stopReading = false;
+
+	void Start () {
+		reader = new StreamReader (scriptAddress);
+		nextLine = reader.ReadLine ();
+		tutorialText.text = nextLine;
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && !reader.EndOfStream && !stopReading){
+			nextLine = reader.ReadLine();
+			tutorialText.text = nextLine;
+			GetComponent<AudioSource>().PlayOneShot(buttonClip, 1.5f);// Plays beep noise.
+		}
+		if (!stopReading && reader.EndOfStream) {
+			reader.Close ();
+			stopReading = true;
+		}
+	}
+}
