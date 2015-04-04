d31f40f4c5d107c5851b9234ad70928a914d758a
Haley <hne3@case.edu>
2015-04-03 22:02:18 -0400

    Working on tiling script

---
+++ Assets/Scripts/Tiler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiler : MonoBehaviour {
+
+	public GameObject tilePrefab;
+	private int width = 10; // Room width
+	private int height = 1; // Room height
+	private float spacing = 0.3f; // Tile spacing;
+	private GameObject tile;
+
+	void Start () {
+		for (int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++){
+				Vector3 position = new Vector3(i, 0, j) * spacing;
+				Instantiate(tilePrefab, position, Quaternion.identity);
+			}
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
