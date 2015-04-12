a103abcd0796d4675def2e5e88ea8e372ebf225d
ianlav <irl@case.edu>
2015-04-12 13:40:17 -0400

    Base
    Builds the grid, using gems, will replace with code fragments at later
    stage

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gem : MonoBehaviour {
+
+	public Gem up, down, left, right;
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public bool sameType(Gem other){
+		return GetComponent<SpriteRenderer> ().sprite == GetComponent<SpriteRenderer> ().sprite;
+	}
+}
--- Assets/Scripts/MIPS/Operand.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class Operand : MonoBehaviour {
-    public abstract string toString();
-}
