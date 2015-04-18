5be6b86c5f147cc9f9c85884f8ad86501afb9197
Haley <hne3@case.edu>
2015-04-18 16:22:49 -0400

    Now correctly spawns instructions.

---
+++ Assets/Scripts/InstructionHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InstructionHandler : MonoBehaviour {
+
+	// Generates instructions, removes instructions, illustrates hazards.
+	public Image[] donuts;
+	public Canvas canvas;
+
+	public void GenerateInstruction(Vector3 position, Vector3 scale){
+		Image[] donutInstances = new Image[5];
+		for (int i = 0; i < donuts.GetLength(0); i++) {
+			donutInstances[i] = Instantiate(donuts[i]) as Image;
+			donutInstances[i].transform.position = new Vector3(position.x + (i * 70), position.y, position.z);
+			donutInstances[i].transform.localScale = scale;
+			donutInstances[i].transform.SetParent(canvas.transform, false);
+			donutInstances[i].gameObject.SetActive(true);
+		}
+	}
+}
--- Assets/Scripts/TextReader.cs
+++ Assets/Scripts/TextReader.cs
@@ -8,6 +8,8 @@ public class TextReader : MonoBehaviour {
 
 	public string scriptAddress;
 
+	public InstructionHandler handler;
+
 	public Text tutorialText;
 	public Image[] donuts;
 
@@ -17,31 +19,47 @@ public class TextReader : MonoBehaviour {
 	private int numItr = 0;
 	private string nextLine;
 	private StreamReader reader;
-	private bool stopReading = false;
+	private bool stopReading;
 
 	void Start () {
 		reader = new StreamReader (scriptAddress);
 		nextLine = reader.ReadLine ();
 		tutorialText.text = nextLine;
+		stopReading = false;
 	}
 	
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space) && !reader.EndOfStream && !stopReading && numItr < 5){
+		if (!stopReading && numItr > 18) {
+			stopReading = true;
+		}
+		if(Input.GetKeyDown(KeyCode.Space) && !stopReading && numItr < 5){
 			nextLine = reader.ReadLine();
 			tutorialText.text = nextLine;
 			GetComponent<AudioSource>().PlayOneShot(buttonClip, 2.5f);// Plays beep noise.
 			numItr++;
 		}
-		if (Input.GetKeyDown(KeyCode.Space) && !reader.EndOfStream && numItr >= 5) { // Makes donuts appear on fifth tap.
+		if (Input.GetKeyDown(KeyCode.Space) && !stopReading && numItr >= 5 && numItr < 10) { // Makes donuts appear on fifth tap.
 			nextLine = reader.ReadLine();
 			tutorialText.text = nextLine;
 			donuts[numItr - 5].gameObject.SetActive(true);
 			GetComponent<AudioSource>().PlayOneShot(donutAppear, 0.1f); //Plays donut appear sound for each donut.
 			numItr++;
 		}
-		if (!stopReading && reader.EndOfStream) {
-			reader.Close ();
-			stopReading = true;
+		if (Input.GetKeyDown (KeyCode.Space) && !stopReading && (numItr >= 10 && numItr <= 18) ) {
+			nextLine = reader.ReadLine();
+			tutorialText.text = nextLine;
+			GetComponent<AudioSource>().PlayOneShot(buttonClip, 2.5f);// Plays beep noise.
+			numItr++;
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && !stopReading && numItr == 16) {
+			for(int i = 0; i < 5; i++){
+				donuts[i].gameObject.SetActive(false);
+			}
+			handler.GenerateInstruction(new Vector3(-170f, 120f, 0), new Vector3(0.5f, 0.5f, 0.5f));
+			handler.GenerateInstruction(new Vector3(-100f, 50f, 0), new Vector3(0.5f, 0.5f, 0.5f));
+			handler.GenerateInstruction(new Vector3(-30f, -20f, 0), new Vector3(0.5f, 0.5f, 0.5f));
+			GetComponent<AudioSource>().PlayOneShot(donutAppear, 0.1f); //Plays donut appear sound.
+			numItr++;
 		}
 	}
 }
