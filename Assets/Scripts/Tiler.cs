6eb156d93f4838063e9ca049d28f277e0840bb75
Haley <hne3@case.edu>
2015-04-27 19:05:52 -0400

    Bakery now spawns tiles in a better format.

--- Assets/Scripts/Tiler.cs
+++ Assets/Scripts/Tiler.cs
@@ -5,24 +5,26 @@ public class Tiler : MonoBehaviour {
 
 	public GameObject tilePrefab;
 	public GameObject wallPrefab;
-	public int width = 2; // Room width
-	public int height = 2; // Room height
+	public int width = 1; // Room width
+	public int height = 1; // Room height
 	private float spacing = 0.3f; // x axis tile spacing
 	private GameObject tile;
 
 	void Start () {
 		// Creates floor tiles.
-		for (int i = -3; i < width; i++) {
-			for(int j = -3; j < height; j++){
+		for (int i = -10; i < width; i++) {
+			for(int j = -10; j < height; j++){
 				Vector3 position = new Vector3(i , j , 0) * spacing;
 				Instantiate(tilePrefab, position, Quaternion.identity);
 			}
 		}
 		//Creates walls.
-		for(int i = -3; i < width; i++){
-			Vector3 position = new Vector3(i, 3, 0) * spacing;
-			Instantiate(wallPrefab, position, Quaternion.identity);
-		}
+		for(int i = -10; i < width; i++){
+			for(int j = 0; j < 3; j++){
+				Vector3 position = new Vector3(i, 3 + j, 0) * spacing;
+				Instantiate(wallPrefab, position, Quaternion.identity);
+			}
+		}	
 	}
 	
 	void Update () {
